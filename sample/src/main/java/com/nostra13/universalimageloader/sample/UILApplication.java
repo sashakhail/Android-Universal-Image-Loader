8d659d791f9c0312ea652150149b92ec6ac3c4ba
sashakhail <sakkkkka@yahoo.com>
2025-03-21 23:23:47 +0500

    Your commit message1

--- sample/src/main/java/com/nostra13/universalimageloader/sample/UILApplication.java
+++ sample/src/main/java/com/nostra13/universalimageloader/sample/UILApplication.java
@@ -33,10 +33,10 @@ public class UILApplication extends Application {
 	@SuppressWarnings("unused")
 	@Override
 	public void onCreate() {
-		if (Constants.Config.DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().build());
-			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().build());
-		}
+//		if (Constants.Config.DEVELOPER_MODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+//			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyDialog().build());
+//			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyDeath().build());
+//		}
 
 		super.onCreate();
 
